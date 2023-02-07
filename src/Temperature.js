1d13c2c7e9ee02b1028507dc3ce33429451f3c6b
NazM64 <nmnazanin@gmail.com>
2023-02-06 18:32:04 -0800

    Added API integration

--- src/Temperature.js
+++ src/Temperature.js
@@ -1,60 +1,94 @@
+import React, { useState } from "react";
+import axios from "axios";
 import "./Temperature.css";
 
 export default function Temperature() {
-  return (
-    <div className="Temperature">
-      <div className="row justify-content-between">
-        <div className="col-4">
-          <h1>Vancouver</h1>
-          <ul className="left">
-            <li id="date-time">Friday 9:00</li>
-            <li id="weather-description">Showers</li>
-            <li>
-              Wind: <span id="wind"></span> km/h
-            </li>
-            <li>
-              Humidity: <span id="humidity"></span>%
-            </li>
-          </ul>
-        </div>
+  const [weatherData, setWeatherData] = useState(null);
+  const [allData, setAllData] = useState(false);
 
-        <div className="col-4 d-flex align-items-center">
-          <img
-            src="http://openweathermap.org/img/wn/09d@2x.png"
-            id="icon-top"
-            alt="weather-logo"
-          />
+  function showWeather(response) {
+    setAllData(true);
+    setWeatherData({
+      maxTemperature: response.data.main.temp_max,
+      minTemperature: response.data.main.temp_min,
+      description: response.data.weather[0].description,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
 
-          <div className="d-flex flex-column d-flex align-items-center">
-            <div>
-              H:<span className="high-degree">6</span>
-              <span className="today-high-temp">
-                <a href="/" id="celsius-link-high" className="active">
-                  °C
-                </a>
-                |
-                <a href="/" id="fahrenheit-link-high">
-                  °F
-                </a>
-              </span>
-            </div>
-            <div>
-              L:<span className="low-degree">1</span>
-              <span className="today-low-temp">
-                <a href="/" id="celsius-link-low" className="active">
-                  °C
-                </a>
-                |
-                <a href="/" id="fahrenheit-link-low">
-                  °F
-                </a>
-              </span>
+  if (allData) {
+    return (
+      <div className="Temperature">
+        <div className="row justify-content-between">
+          <div className="col-4">
+            <h1>Vancouver</h1>
+            <ul className="left">
+              <li id="date-time">Friday 9:00</li>
+              <li id="weather-description" className="text-capitalize">
+                {weatherData.description}
+              </li>
+              <li>
+                Wind: <span id="wind"></span>
+                {weatherData.wind} km/h
+              </li>
+              <li>
+                Humidity: <span id="humidity"></span>
+                {weatherData.humidity}%
+              </li>
+            </ul>
+          </div>
+
+          <div className="col-4 d-flex align-items-center">
+            <img
+              src={weatherData.icon}
+              id="icon-top"
+              alt={weatherData.description}
+            />
+
+            <div className="d-flex flex-column d-flex align-items-center">
+              <div>
+                H:
+                <span className="high-degree">
+                  {weatherData.maxTemperature}
+                </span>
+                <span className="today-high-temp">
+                  <a href="/" id="celsius-link-high" className="active">
+                    °C
+                  </a>
+                  |
+                  <a href="/" id="fahrenheit-link-high">
+                    °F
+                  </a>
+                </span>
+              </div>
+              <div>
+                L:
+                <span className="low-degree">{weatherData.minTemperature}</span>
+                <span className="today-low-temp">
+                  <a href="/" id="celsius-link-low" className="active">
+                    °C
+                  </a>
+                  |
+                  <a href="/" id="fahrenheit-link-low">
+                    °F
+                  </a>
+                </span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div className="weather-forecast" id="forecast"></div>
-    </div>
-  );
+        <div className="weather-forecast" id="forecast"></div>
+      </div>
+    );
+  } else {
+    const apiKey = "ecf6996c8a62ee385547d2ae6dc3dea8";
+    let city = "Vancouver";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    axios.get(apiUrl).then(showWeather);
+
+    return "Loading...";
+  }
 }
